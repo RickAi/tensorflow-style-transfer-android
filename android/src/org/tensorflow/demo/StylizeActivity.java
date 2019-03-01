d72725747025ff6633cb7ef5ffac5d6b4ffb157f
yongbiaoai <yongbiaoai@gmail.com>
2019-03-01 16:23:27 +0800

    add tensorflow code, change fixed image transfer to arbitary image transfer
    tensorflow code refers to:
    https://github.com/elleryqueenhomels/arbitrary_style_transfer
    
    Signed-off-by: yongbiaoai <yongbiaoai@gmail.com>

--- android/src/org/tensorflow/demo/StylizeActivity.java
+++ android/src/org/tensorflow/demo/StylizeActivity.java
@@ -33,6 +33,7 @@ import android.media.Image.Plane;
 import android.media.ImageReader;
 import android.media.ImageReader.OnImageAvailableListener;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.os.SystemClock;
 import android.os.Trace;
 import android.util.Size;
@@ -68,10 +69,16 @@ public class StylizeActivity extends CameraActivity implements OnImageAvailableL
   private static final Logger LOGGER = new Logger();
 
   private TensorFlowInferenceInterface inferenceInterface;
-  private static final String MODEL_FILE = "file:///android_asset/stylize_quantized.pb";
-  private static final String INPUT_NODE = "input";
-  private static final String STYLE_NODE = "style_num";
-  private static final String OUTPUT_NODE = "transformer/expand/conv3/conv/Sigmoid";
+
+  private static final String MODEL_FILE = "file:///android_asset/frozen_model.pb";
+  private static final String INPUT_NODE = "content";
+  private static final String STYLE_NODE = "style";
+  private static final String OUTPUT_NODE = "clip_by_value";
+
+//  private static final String MODEL_FILE = "file:///android_asset/stylize_quantized.pb";
+//  private static final String INPUT_NODE = "input";
+//  private static final String STYLE_NODE = "style_num";
+//  private static final String OUTPUT_NODE = "transformer/expand/conv3/conv/Sigmoid";
   private static final int NUM_STYLES = 26;
 
   private static final boolean SAVE_PREVIEW_BITMAP = false;
@@ -176,11 +183,6 @@ public class StylizeActivity extends CameraActivity implements OnImageAvailableL
           return true;
         }
       };
-
-  @Override
-  public void onCreate(final Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-  }
 
   @Override
   protected int getLayoutId() {
@@ -555,6 +557,23 @@ public class StylizeActivity extends CameraActivity implements OnImageAvailableL
     Trace.endSection();
   }
 
+  private float[] styleFloatValues = new float[512 * 512 * 3];
+  private int[] styleIntValues = new int[512 * 512];;
+
+  @Override
+  public void onCreate(final Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    Bitmap bitmap =
+            getBitmapFromAsset(StylizeActivity.this, "thumbnails/style0.jpg");
+    bitmap.getPixels(styleIntValues, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
+    for (int i = 0; i < styleIntValues.length; ++i) {
+      final int val = styleIntValues[i];
+      styleFloatValues[i * 3] = ((val >> 16) & 0xFF);
+      styleFloatValues[i * 3 + 1] = ((val >> 8) & 0xFF);
+      styleFloatValues[i * 3 + 2] = (val & 0xFF);
+    }
+  }
+
   private void stylizeImage(final Bitmap bitmap) {
     ++frameNum;
     bitmap.getPixels(intValues, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
@@ -576,15 +595,16 @@ public class StylizeActivity extends CameraActivity implements OnImageAvailableL
     } else {
       for (int i = 0; i < intValues.length; ++i) {
         final int val = intValues[i];
-        floatValues[i * 3] = ((val >> 16) & 0xFF) / 255.0f;
-        floatValues[i * 3 + 1] = ((val >> 8) & 0xFF) / 255.0f;
-        floatValues[i * 3 + 2] = (val & 0xFF) / 255.0f;
+        floatValues[i * 3] = ((val >> 16) & 0xFF);
+        floatValues[i * 3 + 1] = ((val >> 8) & 0xFF);
+        floatValues[i * 3 + 2] = (val & 0xFF);
       }
     }
 
     // Copy the input data into TensorFlow.
     inferenceInterface.feed(INPUT_NODE, floatValues, 1, bitmap.getWidth(), bitmap.getHeight(), 3);
-    inferenceInterface.feed(STYLE_NODE, styleVals, NUM_STYLES);
+    inferenceInterface.feed(STYLE_NODE, styleFloatValues, 1, 512, 512, 3);
+    //    inferenceInterface.feed(STYLE_NODE, styleVals, NUM_STYLES);
 
     // Execute the output node's dependency sub-graph.
     inferenceInterface.run(new String[] {OUTPUT_NODE}, isDebug());
@@ -595,9 +615,9 @@ public class StylizeActivity extends CameraActivity implements OnImageAvailableL
     for (int i = 0; i < intValues.length; ++i) {
       intValues[i] =
           0xFF000000
-              | (((int) (floatValues[i * 3] * 255)) << 16)
-              | (((int) (floatValues[i * 3 + 1] * 255)) << 8)
-              | ((int) (floatValues[i * 3 + 2] * 255));
+              | (((int) (floatValues[i * 3])) << 16)
+              | (((int) (floatValues[i * 3 + 1])) << 8)
+              | ((int) (floatValues[i * 3 + 2]));
     }
 
     bitmap.setPixels(intValues, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
