e632555dc2360e5ba81a5796747f983724d9d7ab
yongbiaoai <yongbiaoai@gmail.com>
2019-03-01 17:11:48 +0800

    add tensorboard
    Signed-off-by: yongbiaoai <yongbiaoai@gmail.com>

--- tensorflow/train.py
+++ tensorflow/train.py
@@ -8,10 +8,9 @@ import tensorflow as tf
 from style_transfer_net import StyleTransferNet
 from utils import get_train_images
 
+STYLE_LAYERS = ('relu1_1', 'relu2_1', 'relu3_1', 'relu4_1')
 
-STYLE_LAYERS  = ('relu1_1', 'relu2_1', 'relu3_1', 'relu4_1')
-
-TRAINING_IMAGE_SHAPE = (256, 256, 3) # (height, width, color_channels)
+TRAINING_IMAGE_SHAPE = (256, 256, 3)  # (height, width, color_channels)
 
 EPOCHS = 4
 EPSILON = 1e-5
@@ -21,7 +20,7 @@ LR_DECAY_RATE = 5e-5
 DECAY_STEPS = 1.0
 
 
-def train(style_weight, content_imgs_path, style_imgs_path, encoder_path, 
+def train(style_weight, content_imgs_path, style_imgs_path, encoder_path,
           model_save_path, debug=False, logging_period=100):
     if debug:
         from datetime import datetime
@@ -30,12 +29,12 @@ def train(style_weight, content_imgs_path, style_imgs_path, encoder_path,
     # guarantee the size of content and style images to be a multiple of BATCH_SIZE
     num_imgs = min(len(content_imgs_path), len(style_imgs_path))
     content_imgs_path = content_imgs_path[:num_imgs]
-    style_imgs_path   = style_imgs_path[:num_imgs]
+    style_imgs_path = style_imgs_path[:num_imgs]
     mod = num_imgs % BATCH_SIZE
     if mod > 0:
         print('Train set has been trimmed %d samples...\n' % mod)
         content_imgs_path = content_imgs_path[:-mod]
-        style_imgs_path   = style_imgs_path[:-mod]
+        style_imgs_path = style_imgs_path[:-mod]
 
     # get the traing image shape
     HEIGHT, WIDTH, CHANNELS = TRAINING_IMAGE_SHAPE
@@ -44,7 +43,7 @@ def train(style_weight, content_imgs_path, style_imgs_path, encoder_path,
     # create the graph
     with tf.Graph().as_default(), tf.Session() as sess:
         content = tf.placeholder(tf.float32, shape=INPUT_SHAPE, name='content')
-        style   = tf.placeholder(tf.float32, shape=INPUT_SHAPE, name='style')
+        style = tf.placeholder(tf.float32, shape=INPUT_SHAPE, name='style')
 
         # create the style transfer net
         stn = StyleTransferNet(encoder_path)
@@ -57,7 +56,7 @@ def train(style_weight, content_imgs_path, style_imgs_path, encoder_path,
 
         # pass the generated_img to the encoder, and use the output compute loss
         generated_img = tf.reverse(generated_img, axis=[-1])  # switch RGB to BGR
-        generated_img = stn.encoder.preprocess(generated_img) # preprocess image
+        generated_img = stn.encoder.preprocess(generated_img)  # preprocess image
         enc_gen, enc_gen_layers = stn.encoder.encode(generated_img)
 
         # compute the content loss
@@ -67,15 +66,15 @@ def train(style_weight, content_imgs_path, style_imgs_path, encoder_path,
         style_layer_loss = []
         for layer in STYLE_LAYERS:
             enc_style_feat = stn.encoded_style_layers[layer]
-            enc_gen_feat   = enc_gen_layers[layer]
+            enc_gen_feat = enc_gen_layers[layer]
 
             meanS, varS = tf.nn.moments(enc_style_feat, [1, 2])
-            meanG, varG = tf.nn.moments(enc_gen_feat,   [1, 2])
+            meanG, varG = tf.nn.moments(enc_gen_feat, [1, 2])
 
             sigmaS = tf.sqrt(varS + EPSILON)
             sigmaG = tf.sqrt(varG + EPSILON)
 
-            l2_mean  = tf.reduce_sum(tf.square(meanG - meanS))
+            l2_mean = tf.reduce_sum(tf.square(meanG - meanS))
             l2_sigma = tf.reduce_sum(tf.square(sigmaG - sigmaS))
 
             style_layer_loss.append(l2_mean + l2_sigma)
@@ -85,12 +84,18 @@ def train(style_weight, content_imgs_path, style_imgs_path, encoder_path,
         # compute the total loss
         loss = content_loss + style_weight * style_loss
 
+        tf.summary.scalar("content_loss", content_loss)
+        tf.summary.scalar("style_loss", style_weight * style_loss)
+        tf.summary.scalar("loss", loss)
+        merged_summary_op = tf.summary.merge_all()
+
         # Training step
         global_step = tf.Variable(0, trainable=False)
         learning_rate = tf.train.inverse_time_decay(LEARNING_RATE, global_step, DECAY_STEPS, LR_DECAY_RATE)
         train_op = tf.train.AdamOptimizer(learning_rate).minimize(loss, global_step=global_step)
 
         sess.run(tf.global_variables_initializer())
+        summary_writer = tf.summary.FileWriter("logs", graph=tf.get_default_graph())
 
         # saver
         saver = tf.train.Saver(max_to_keep=10)
@@ -113,14 +118,14 @@ def train(style_weight, content_imgs_path, style_imgs_path, encoder_path,
 
                 for batch in range(n_batches):
                     # retrive a batch of content and style images
-                    content_batch_path = content_imgs_path[batch*BATCH_SIZE:(batch*BATCH_SIZE + BATCH_SIZE)]
-                    style_batch_path   = style_imgs_path[batch*BATCH_SIZE:(batch*BATCH_SIZE + BATCH_SIZE)]
+                    content_batch_path = content_imgs_path[batch * BATCH_SIZE:(batch * BATCH_SIZE + BATCH_SIZE)]
+                    style_batch_path = style_imgs_path[batch * BATCH_SIZE:(batch * BATCH_SIZE + BATCH_SIZE)]
 
                     content_batch = get_train_images(content_batch_path, crop_height=HEIGHT, crop_width=WIDTH)
-                    style_batch   = get_train_images(style_batch_path,   crop_height=HEIGHT, crop_width=WIDTH)
+                    style_batch = get_train_images(style_batch_path, crop_height=HEIGHT, crop_width=WIDTH)
 
                     # run the training step
-                    sess.run(train_op, feed_dict={content: content_batch, style: style_batch})
+                    sess.run([train_op, merged_summary_op], feed_dict={content: content_batch, style: style_batch})
 
                     step += 1
 
@@ -132,12 +137,15 @@ def train(style_weight, content_imgs_path, style_imgs_path, encoder_path,
 
                         if is_last_step or step == 1 or step % logging_period == 0:
                             elapsed_time = datetime.now() - start_time
-                            _content_loss, _style_loss, _loss = sess.run([content_loss, style_loss, loss], 
-                                feed_dict={content: content_batch, style: style_batch})
+                            _content_loss, _style_loss, _loss, summary = sess.run([content_loss, style_loss, loss],
+                                                                                  feed_dict={content: content_batch,
+                                                                                             style: style_batch})
 
+                            summary_writer.add_summary(summary, step * BATCH_SIZE + batch)
                             print('step: %d,  total loss: %.3f,  elapsed time: %s' % (step, _loss, elapsed_time))
                             print('content loss: %.3f' % (_content_loss))
-                            print('style loss  : %.3f,  weighted style loss: %.3f\n' % (_style_loss, style_weight * _style_loss))
+                            print('style loss  : %.3f,  weighted style loss: %.3f\n' % (
+                            _style_loss, style_weight * _style_loss))
         except Exception as ex:
             saver.save(sess, model_save_path, global_step=step)
             print('\nSomething wrong happens! Current model is saved to <%s>' % model_save_path)
@@ -150,4 +158,6 @@ def train(style_weight, content_imgs_path, style_imgs_path, encoder_path,
             elapsed_time = datetime.now() - start_time
             print('Done training! Elapsed time: %s' % elapsed_time)
             print('Model is saved to: %s' % model_save_path)
-
+            print("\nRun the command line:\n" \
+                  "--> tensorboard --logdir=./logs " \
+                  "\nThen open http://0.0.0.0:6006/ into your web browser")
